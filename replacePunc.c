6388f7d90acadd42a9fa7445eaf20fc940d4a158
AaronBester <bestera@uoguelph.ca>
2019-11-03 15:11:24 -0500

    Added Makefile

--- replacePunc.c
+++ replacePunc.c
@@ -6,15 +6,15 @@ int replacePunc(char *line){
   int lineLength = 0;
   int character = 0;
   int i = 0;
-  int total = 0;
+  int commasRemoved = 0;
 
   lineLength = strlen (line);
 
   for(i=0; i<lineLength;i++){
     character = line[i];
-    if((character>32 && character < 48) || (character >57 && character <65) ||( character >90 && character < 97) || (character > 122 && character < 256)){
+    if(character == 44){ 
       line[i] = 32;
-      total++;
+      commasRemoved++;
     }
   }
 
